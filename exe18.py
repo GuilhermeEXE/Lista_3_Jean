1148b520e9129c49442b1915d0ad036ca1bd5b3c
Guilherme_Parucci <107225000+GuilhermeEXE@users.noreply.github.com>
2022-06-22 10:34:09 -0400

    Update exe18.py

--- exe18.py
+++ exe18.py
@@ -2,39 +2,15 @@
 #formato D de mesPorExtenso de AAAA. Opcionalmente, valide a data e retorne NULL caso a data
 #seja inválida.
 
-def validaData(data):
-	global m,d,a
-	data = data.split('/')
-	m=data[1]
-	m=int(m)
-	d=data[0]
-	d=int(d)
-	a=data[2]
-	a=int(a)
-	if m>0 or m<13:
-		if m==2:
-			if d==29:
-				if(a%4 == 0 and a%100!= 0) or (a%400==0):
-					return "VALIDA"
-				else:
-					return "NULL"
-			elif m==1 or m==3 or m==5 or m==7 or m==8 or m==10 or m==12:
-					if d>0 and d<=31:
-						return "VALIDA"
-					else:
-						return "NULL"
-			elif m==4 or m==6 or m==9 or m==11:
-						if d>0 and d<=30:
-							return "VALIDA"
-						else:
-							return "NULL"
-			else:
-						return("NULL")
-data=input("Digite uma data com o seguinte formato dd/mm/aaaa: ")
-mes=['','Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro']
-valida = validaData(data)
+def dE(dia, mes, ano):
+    mE = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", 'Setembro', "Outubro",
+               'Novembro', 'Dezembro']
 
-if valida=="VALIDA":
-	print("Data com mês por extenso: ",str(d),"de",str(mes[m]),"de",str(a))
-else:
-	print("Data inválida!")
+
+    if mes or dia > 12 or dia or mes < 1:
+        return print("NULL")
+    else:
+        return print(f'Data: Dia {dia}, de {mE[mes]} de {ano} ')
+
+
+print(dE(int(input('Dia: ')), int(input('Mês: ')), int(input('Ano: '))), end='')

36fde4ad64cfe065dc2e5dd3a28b5ffd67ce042a
Guilherme_Parucci <107225000+GuilhermeEXE@users.noreply.github.com>
2022-06-22 10:49:06 -0400

    Update exe06.py

--- exe06.py
+++ exe06.py
@@ -1,11 +1,43 @@
 #6) Faça um programa que leia 2 strings e informe o conteúdo delas seguido do seu comprimento. Informe
 #também se as duas strings possuem o mesmo comprimento e são iguais ou diferentes no conteúdo.
 
-string_1 = input("Digite a primeira string: ")
-string_2 = input("Digite a segunda string: ")
+p_frase = str(input('Digite uma frase: ').upper())
+s_frase = str(input('Digitee outra frase: ').upper())
+resp = []
+difer_n1 = []
+difer_n2 = []
 
-tamanho_str_1 = len(string_1)
-tamanho_str_2 = len(string_2)
 
-print(string_1 + " " + str(tamanho_str_1))
-print(string_2 + " " + str(tamanho_str_2))
+if len(p_frase) != len(s_frase):
+    print(f'Primeira frase: "{p_frase}"')
+    print(f'Segunda frase: "{s_frase}"')
+    print(f'{p_frase}: tamanho: {len(p_frase)} caractere')
+    print(f'{s_frase}: tamanho: {len(s_frase)} caractere')
+    print('As duas frases são diferentes em tamanho.')
+    print('As duas frases possuem conteúdos diferentes')
+
+else:
+
+ 
+    for i in range(len(p_frase)):
+        resp.append(p_frase[i] in s_frase[i])
+
+    
+        if False in resp:
+            difer_n1.append(p_frase[i])
+            difer_n2.append(s_frase[i])
+            resp[i] = True
+
+
+    print(f'Primeira frase: {p_frase}')
+    print(f'segunda frase: {s_frase}')
+    print(f'{p_frase}: tamanho: {len(p_frase)} caractere')
+    print(f'{s_frase}: tamanho: {len(s_frase)} caractere')
+    print('As duas frases tem o mesmo tamanho.')
+    for i in range(len(p_frase)):
+        resp.append(p_frase[i] in s_frase[i])
+    if False in resp:
+        print(f'As frases são diferentes:{difer_n1} != {difer_n2} '.replace("[", '').replace(']', '').replace("'", '').replace(
+            "'", ''))
+    else:
+        print(f'As frases são iguais:')
